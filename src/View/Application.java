c22132ff8ca5b47bf6f79c562b38ff49af6ea246
Lord-Skoog <jacksonS900@lpsk12.org>
2024-02-07 11:19:54 -0700

    monkey

--- src/View/Application.java
+++ src/View/Application.java
@@ -21,10 +21,13 @@ public class Application {
     }
 
     public Application() {
-
-
-
-
+        setTitle("Sample Polygon Drawing Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
 
     }
 
