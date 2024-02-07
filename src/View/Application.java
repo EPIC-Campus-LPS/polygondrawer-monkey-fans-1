772cbfe28bfa4bfbc6335c7ca4c592bbafb60607
benjamina107 <benjamina107@lpsk12.org>
2024-02-07 10:47:30 -0700

    hkf

---
+++ src/View/Application.java
@@ -0,0 +1,12 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Application {
+    private JPanel contentPane;
+
+    public static void Main(String[] args) {
+        EventQueue.invokeLater();
+    }
+}
