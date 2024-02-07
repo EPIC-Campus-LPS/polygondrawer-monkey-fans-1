3e9474f00b7070748f36ded85a6de2685cb377a4
benjamina107 <benjamina107@lpsk12.org>
2024-02-07 11:49:01 -0700

    Monkey🙈🙈🙈🙈🙈🙈🚫🚫🚫🚫🚫

--- src/View/Application.java
+++ src/View/Application.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 public class Application extends JFrame{
     private JPanel contentPane;
@@ -20,16 +21,19 @@ public class Application extends JFrame{
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
 
-        JMenu mnPolygon = new JMenu("Polygon")
+        JMenu mnPolygon = new JMenu("Polygon");
         menuBar.add(mnPolygon);
-//monkey
+
         JMenuItem mntmReset = new JMenuItem("Reset");
         mntmReset.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
         mnPolygon.add(mntmReset);
 
-        JMenu mnEdit = new Jmenu("Edit");
+        JMenu mnEdit = new JMenu("Edit");
         menuBar.add(mnEdit);
 
+        JMenuItem mntmRemoveLast = new JMenuItem("Remove Last");
+        mntmRemoveLast.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, 0));
+        mnEdit.add(mntmRemoveLast);
 
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
