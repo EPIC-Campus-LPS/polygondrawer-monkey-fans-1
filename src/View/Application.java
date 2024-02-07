8563d1dfcf863b8c9d0ec3bac55589b9ac701599
Lord-Skoog <jacksonS900@lpsk12.org>
2024-02-07 11:44:06 -0700

    monkey

--- src/View/Application.java
+++ src/View/Application.java
@@ -3,6 +3,7 @@ package View;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.InputEvent;
 
 public class Application extends JFrame{
     private JPanel contentPane;
@@ -15,6 +16,21 @@ public class Application extends JFrame{
         setTitle("Sample Polygon Drawing Application");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 450, 300);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu mnPolygon = new JMenu("Polygon")
+        menuBar.add(mnPolygon);
+
+        JMenuItem mntmReset = new JMenuItem("Reset");
+        mntmReset.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
+        mnPolygon.add(mntmReset);
+
+        JMenu mnEdit = new Jmenu("Edit");
+        menuBar.add(mnEdit);
+
+
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         contentPane.setLayout(new BorderLayout(0, 0));
