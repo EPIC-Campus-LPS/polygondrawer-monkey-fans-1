4dbf48572d9ec8d98fe98eb6bac3b6e141c53012
benjamina107 <benjamina107@lpsk12.org>
2024-02-09 10:47:46 -0700

    Monkey🙈🙈🙈🙈🙈🙈🚫🚫🚫🚫🚫

--- src/View/View/PolygonDrawer.java
+++ src/View/View/PolygonDrawer.java
@@ -3,9 +3,14 @@ package View;
 import Model.Model;
 
 import javax.swing.JPanel;
+import java.awt.*;
+import java.util.Iterator;
+
+import java.awt.Graphics;
+import java.awt.Polygon;
 
 public class PolygonDrawer extends JPanel {
-
+    public static final int Radius = 2;
     Model model;
 
     /**
@@ -19,7 +24,7 @@ public class PolygonDrawer extends JPanel {
 
 
         public void paintComponent(Graphics g) {
-            super.PaintComponent(g);
+            super.paintComponent(g);
 
             Iterator<Polygon> it = model.iterator();
             while (it.hasNext()) {
@@ -30,10 +35,10 @@ public class PolygonDrawer extends JPanel {
                     for (int i = 0; i <poly.npoints-1; i++) {
                         g.drawLine(poly.xpoints[i], poly.ypoints[i], poly.xpoints[i+1], poly.ypoints[i+1]);
                     }
-                    g.DrawLine(poly.xpoints[poly.npoints-1], poly.ypoints[poly.npoints-1], poly.xpoints[0], poly.ypoints[0]);
+                    g.drawLine(poly.xpoints[poly.npoints-1], poly.ypoints[poly.npoints-1], poly.xpoints[0], poly.ypoints[0]);
                 } else {
                     for (int i = 0; i <poly.npoints; i++)  {
-                        g.drawOval(poly.xpoints[i] - Radius, poly.ypoints[i] - Radius, 2*Radius, 2*Radius)
+                        g.drawOval(poly.xpoints[i] - Radius, poly.ypoints[i] - Radius, 2*Radius, 2*Radius);
                     }
                 }
             }
