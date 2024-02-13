579059e206bdf8146914336e4723264490afeeca
benjamina107 <benjamina107@lpsk12.org>
2024-02-13 10:10:33 -0700

    Monkey🙈🙈🙈🙈🙈🙈🚫🚫🚫🚫🚫

--- src/View/View/Application.java
+++ src/View/View/Application.java
@@ -61,6 +61,7 @@ public class Application extends JFrame{
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         contentPane.setLayout(new BorderLayout(0, 0));
         setContentPane(contentPane);
+        contentPane.setBackground(Color.red);
 
         contentPane.addMouseListener(new MouseAdapter() {
 
